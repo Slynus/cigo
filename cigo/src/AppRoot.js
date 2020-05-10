1c331df86d963d21bd74cc1c841734324eec1d70
Slynus <lustucru86@gmail.com>
2020-05-10 17:15:45 +0200

    Dumb component done

--- cigo/src/AppRoot.js
+++ cigo/src/AppRoot.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import { Container, Box, TextField, Typography, Fab, Hidden } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import TextField from '@material-ui/core/TextField';
-import { Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { createMuiTheme, ThemeProvider, responsiveFontSizes, makeStyles } from '@material-ui/core/styles';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+
+const useStyles = makeStyles({
+  fab: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+  }
+});
+
 
 function AppRoot() {
   return (
-    // Wrap all application (for background usage later)
-    <Container maxWidth={false}>
-      {/* Wrap all app content */}
-      <Box my={6}>
-        <Container component="main" maxWidth="md">
-          <SmartMeteoWrapper/>
-        </Container>
-      </Box>
-    </Container>
+    <ThemeProvider theme={theme}>
+
+      {/* // Wrap all application (for background usage later) */}
+      <Container maxWidth={false}>
+        {/* Wrap all app content */}
+        <Box my={6}>
+          <Container component="main" maxWidth="md">
+            <SmartMeteoWrapper />
+          </Container>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
@@ -23,6 +40,7 @@ function AppRoot() {
  * Orchestrate State for application
  */
 function SmartMeteoWrapper(props) {
+  const classes = useStyles();
 
   let cityList = [
     "Rennes",
@@ -34,26 +52,43 @@ function SmartMeteoWrapper(props) {
   return (
     <React.Fragment>
       <Box display="flex" flexDirection="column">
-
-      <SearchAutocomplete list={cityList} label="Ville" />
-
-      {/* Texte De Pluie Dynamique
-      Graphe de Pluie
-      Floating Actiono Button */}
-
+        <Box my={1}>
+          <SearchAutocomplete list={cityList} label="Ville" />
+        </Box>
+        <Box my={1}>
+          <MeteoText text="Pluie faible dans 15 minutes." />
+        </Box>
+        <Box my={1}>
+          <div style={{ padding: '50px', backgroundColor: 'aqua' }}></div>
+        </Box>
+        <Hidden mdUp>
+          <Fab color="primary" aria-label="search" className={classes.fab}>
+            <SearchIcon />
+          </Fab>
+        </Hidden>
       </Box>
-
     </React.Fragment>
   )
 }
 
+
+
+function MeteoText(props) {
+  return (
+    <Typography variant="h1" component="h1" gutterBottom>
+      {props.text}
+    </Typography>
+  );
+}
+
 function SearchAutocomplete(props) {
   return (
-    <Autocomplete 
-    options={props.list}
-    fullWidth={true}
-    disableClearable
-    renderInput={(params) => <TextField {...params} label={props.label} variant="outlined" InputProps={{ ...params.InputProps, type: 'search' }}/>}
+    <Autocomplete
+      options={props.list}
+      fullWidth={true}
+      disableClearable
+      freeSolo
+      renderInput={(params) => <TextField {...params} label={props.label} variant="outlined" InputProps={{ ...params.InputProps, type: 'search' }} />}
     />
   );
 }
