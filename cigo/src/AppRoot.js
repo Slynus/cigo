1510e8f2a0b636594e6a2f59c743631596c181ef
Slynus <lustucru86@gmail.com>
2020-05-15 22:17:09 +0200

    MVP Done

--- cigo/src/AppRoot.js
+++ cigo/src/AppRoot.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { Container, Box, TextField, Typography, Fab, Hidden, InputAdornment } from '@material-ui/core';
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import SearchIcon from '@material-ui/icons/Search';
-import CloseIcon from '@material-ui/icons/Close';
 import { createMuiTheme, ThemeProvider, responsiveFontSizes, makeStyles } from '@material-ui/core/styles';
 
 let theme = createMuiTheme();
@@ -43,59 +42,30 @@ function AppRoot() {
  */
 function SmartMeteoWrapper(props) {
 
-  // const defaultCity = "Rennes";
-
-  // let defaultCityList = [
-  //   "Rennes",
-  //   "Poitiers",
-  //   "Paris"
-  // ];
-
-  let defaultCityList = [
-    {
-      label:"Rennes",
-      id:352380
-    },
-    {
-      label:"Poitiers",
-      id:861940
-    },
-    {
-      label:"Paris",
-      id:751010
-    }
-  ];
-
   const defaultCity = {
-    label:"Rennes",
-    id:352380
+    label: "Rennes",
+    id: 352380
   }
 
-
+  const texteSansPluie = "Vous pouvez sortir sans risque ! 😎";
+  const texteAvecPluie = "Attention pluie prévue ! 🌧️";
 
   const [currentCity, setCurrentCity] = useState(defaultCity);
-  const [cityList, setCityList] = useState(defaultCityList);
+  const [displayedCity, setDisplayedCity] = useState(currentCity);
+  const [cityList, setCityList] = useState([]);
 
-  // let cityList = defaultCityList;
+  const [meteoText, setMeteoText] = useState("");
 
   useEffect(() => {
-    document.title = `${currentCity}`;
+    console.log("Use Effect ! Search");
 
-    // if (currentCity.length === 2 || currentCity === defaultCity) {
-      cityListUpdate(currentCity);
-    // }
-
-    //MAJ Text
-    //MAJ Graph 
-
+    if (currentCity.label.length === 2 || currentCity.label === defaultCity.label) {
+      cityListUpdate(currentCity.label);
+    }
 
     async function cityListUpdate(citySearch) {
       const fetchResult = await cityFetch(citySearch);
       const jsonResult = await fetchResult.json();
-
-
-      const slugList = jsonResult.map(el => el.slug);
-
 
       const jsonTransformed = jsonResult.map(el => {
         let label = el.nomAffiche.split(' ');
@@ -109,28 +79,58 @@ function SmartMeteoWrapper(props) {
         }
       });
 
-
-      console.log(jsonTransformed);
       setCityList(jsonTransformed);
     }
 
-  }, [currentCity]);
+  }, [currentCity, defaultCity.label]);
 
+  useEffect(() => {
+    console.log("Use Effect ! Meteo");
+    document.title = `${displayedCity.label}`;
+
+    meteoUpdate(displayedCity.id);
+
+    async function meteoUpdate(cityId) {
+      const fetchResult = await meteoFetch(cityId);
+      const jsonResult = await fetchResult.json();
+
+      const levelPluie = jsonResult.dataCadran.map(el => el.niveauPluie);
+
+      // majMeteooText()
+      // majGraph()
+
+      if (levelPluie[0] > 1) {
+        setMeteoText(texteAvecPluie);
+      } else {
+        setMeteoText(texteSansPluie);
+      }
+    }
+
+  }, [displayedCity, meteoText]);
 
 
   function searchHandleChange(event, value) {
-    const newCity = {
-      label:value
+    const foundCity = cityList.find(el => el.label.toLowerCase() === value.toLowerCase());
+
+    if (foundCity) {
+      setCurrentCity(foundCity);
+      setDisplayedCity(foundCity);
+    } else {
+      const newCity = {
+        label: value
+      }
+      setCurrentCity(newCity);
     }
-
-    // TO DO HERE (check include ?) ( et apres if id déclenchement call API pluie et if pas d'id change style) dans le useEffect? 
-    setCurrentCity(newCity);
   }
-
 
   function cityFetch(citySearch) {
     const API_URL = `${process.env.API_HOST || ""}/mf3-rpc-portlet/rest/lieu/facet/pluie/search`;
     return fetch(`${API_URL}/${citySearch}`);
+  }
+
+  function meteoFetch(cityId) {
+    const API_URL = `${process.env.API_HOST || ""}/mf3-rpc-portlet/rest/pluie`;
+    return fetch(`${API_URL}/${cityId}`);
   }
 
   return (
@@ -139,6 +139,7 @@ function SmartMeteoWrapper(props) {
       searchInputValue={currentCity}
       searchCityList={cityList}
       searchHandleChange={searchHandleChange}
+      meteoText={meteoText}
     />
   );
 }
@@ -158,7 +159,7 @@ function DumbMeteoWrapper(props) {
             label="Ville" />
         </Box>
         <Box my={1}>
-          <MeteoText text="Pluie faible dans 15 minutes." />
+          <MeteoText text={props.meteoText} />
         </Box>
         <Box my={1}>
           <div style={{ padding: '50px', backgroundColor: 'aqua' }}></div>
@@ -189,20 +190,20 @@ function SearchAutocomplete(props) {
     ignoreAccents: true,
   });
 
-  let testIcon = <SearchIcon fontSize="small" />;
   return (
     <Autocomplete
-    defaultValue={props.defaultValue}
-    // inputValue={props.inputValue}
-    // defaultValue={props.defaultValue.label}
-    // inputValue={props.inputValue.label}
-    getOptionLabel={opt=>opt.label}
-    options={props.list}
+      defaultValue={props.defaultValue.label}
+      // inputValue={props.inputValue}
+      // defaultValue={props.defaultValue.label}
+      // inputValue={props.inputValue.label}
+
+      // getOptionLabel={opt => opt.label}
+      options={props.list.map(el => el.label)}
 
       onInputChange={props.handleChange}
       selectOnFocus
       handleHomeEndKeys
-      debug
+
       freeSolo
       filterOptions={filterOptions}
       renderInput={(params) =>
