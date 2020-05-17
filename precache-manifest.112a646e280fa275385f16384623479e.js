96deb1a97b395156616c7e3b5f83e99d7c6fea44
Slynus <lustucru86@gmail.com>
2020-05-18 01:29:45 +0200

    Updates

---
+++ precache-manifest.112a646e280fa275385f16384623479e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e710ddf060371e932a6ef1db686d4070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2088af6e31f54430dad",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "96a2850987ce632dbf98",
+    "url": "/static/js/2.d35e881f.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.d35e881f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2088af6e31f54430dad",
+    "url": "/static/js/main.8f0e192c.chunk.js"
+  },
+  {
+    "revision": "406a4f51feb7f98ce426",
+    "url": "/static/js/runtime-main.5ca4f023.js"
+  }
+]);
